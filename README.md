0c2b57708c3e01da736cd5b3004a051557ee512a
Patrick Kutch <patrick.kutch@gmail.com>
2022-10-05 15:07:07 -0700

    analyzer how has a --last option

--- README.md
+++ README.md
@@ -2,3 +2,37 @@
 
 Simple home network monitor.
 
+My internet takes a dump at inconvenient times (like meetings) and I never know if it is the internet provider or my home router that has been running 24x7 for years.
+
+So I created this litle tool to ping a bunch of targets (that you specify in a config file) and write the results to a CSV file that you can then open in Excel and graph easily.
+
+usage: NetWatch.py [-h] -c CONFIG -o OUTPUT [-q]
+
+Home Network Watcher
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CONFIG, --config CONFIG
+                        Configuration file to use.
+  -o OUTPUT, --output OUTPUT
+                        file to write to
+  -q, --quiet           will not print out status
+  
+You can specify any targets as you like in the config file, an example is provided (NetWatch.ini).
+
+# nwAnalyzer
+The 2nd part of the tool, allows you to run a quick analysis of the data in the CSV file. 
+
+usage: nwAnalyzer.py [-h] -c CONFIG -i INPUT [-l LAST]
+
+Analyzer for the Home Network Watcher
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CONFIG, --config CONFIG
+                        Configuration file to use.
+  -i INPUT, --input INPUT
+                        The .CSV file to read data from.
+  -l LAST, --last LAST  will run the report on the data from the last # of seconds specified you can specify an
+                        optional suffix (with no space): s: seconds (default) m: minutes (example: 32m) h: hours
+                        (example: 12h) d: days (example: 3d)
--- nwAnalyzer.py
+++ nwAnalyzer.py
@@ -7,6 +7,7 @@ LICENSE file in the root directory of this source tree.
 """
 import os
 from datetime import datetime
+from datetime import timedelta
 import argparse
 import NetWatch
 
@@ -17,10 +18,20 @@ __maintainer__ = "Patrick Kutch"
 __email__ = "Patrick.Kutch@gmail.com"
 __status__ = "Production"
 
+# global var
 config = None
 
 
-def getDateTimeByRow(dataSet, rowNumber) -> tuple:
+def getDateTimeByRow(dataSet, rowNumber) -> datetime:
+    """Retrieves date and time from a specified row.
+
+    Parameters:
+    dataSet (pandas ds): the data from the CSV fie
+    rowNumber (int): which row to get the data from
+
+    Returns:
+    datetime: the datetime object represeting the date and time of the row
+    """
     try:
         date = dataSet["Date"][rowNumber].strip()
         time = dataSet["Time"][rowNumber].strip()
@@ -31,7 +42,14 @@ def getDateTimeByRow(dataSet, rowNumber) -> tuple:
     return datetime.strptime(dateTimeStr, "%m/%d/%Y%H:%M:%S")
 
 
-def doReport(dataSet, target: str) -> None:
+def doReportForTarget(dataSet, target: str) -> None:
+    """Prints a report for the specified target
+
+    Parameters:
+    dataSet (pandas ds): the data from the CSV fie
+    target (str): which target column to do the report on
+    """
+
     targDsAll = dataSet[target]
     totalPoints = len(targDsAll)
     # do not include the failures
@@ -65,10 +83,92 @@ def doReport(dataSet, target: str) -> None:
     print()
 
 
+def trimDataSetByLast(dataSet, timePeriodStr: int):
+    """Returns a subset of the data from the CSV file, the last # of seconds worth
+
+    Parameters:
+    dataSet (pandas ds): the data from the CSV fie
+    timePeriod (str): how long to go 'back' to.  Can have 's'|'m'|'h' for seconds, minutes hours
+
+    Returns:
+    pandas ds: the last specified # of seconds of the input dataset
+    """
+
+    startTime = getDateTimeByRow(dataSet, 0)
+    endTime = getDateTimeByRow(dataSet, dataSet.index[-1])
+    try:
+        timePeriod = int(timePeriodStr)
+
+    except ValueError:  # user specifed units
+        try:
+            timePeriod = int(timePeriodStr[:-1])
+        except ValueError:  # bad juju
+            print("invalid -l/--last  time period specified: {}".format(timePeriodStr))
+            return (False, None)
+
+        unit = timePeriodStr[-1].lower()
+        if unit == "s":  # seconds
+            pass  # already calc based on seconds
+
+        elif unit == "m":  # minutes
+            timePeriod *= 60
+
+        elif unit == "h":  # hours
+            timePeriod *= 60 * 60
+
+        elif unit == "d":  # days
+            timePeriod *= 60 * 60 * 24
+
+        else:
+            print("invalid -l/--last  time period specified: {}".format(timePeriodStr))
+            return (False, None)
+
+    roughStartTime = endTime - timedelta(seconds=timePeriod)
+
+    if roughStartTime > startTime:
+        # specified time less than what is in file, so let's find and trim
+        for index in range(dataSet.index[-1], 0, -1):
+            if getDateTimeByRow(dataSet, index) < roughStartTime:
+                dataSet = dataSet[index + 1 :]
+                break  # found cut off point and trimmed
+
+    return (True, dataSet)
+
+
+def reportPublicIP(dataSet):
+    """Prints a report of the recored public IP
+
+    Parameters:
+    dataSet (pandas ds): the data from the CSV fie
+    """
+    dataCol = dataSet["Public IP"]
+    ipList = dataCol.unique()
+    if len(ipList) > 1:
+        prevIP = dataCol[0]
+        print("\nPublic IP Address Changes: [Current IP-->{}".format(ipList[-1]))
+        for index, currIP in enumerate(ipList):
+            if currIP != prevIP:
+                print(
+                    "\t{}-->{}\t[{}]".format(
+                        prevIP, currIP, getDateTimeByRow(dataSet, index)
+                    )
+                )
+                prevIP = currIP
+
+    else:
+        print(
+            "\nNo Public IP Address Changes Detected: [Current IP-->{}".format(ipList)
+        )
+
+    print()
+
+
 def main():
     print(f"NetWatch Analyzer v{__version__}")
 
-    parser = argparse.ArgumentParser(description="Home Network Watcher")
+    parser = argparse.ArgumentParser(
+        description="Analyzer for the Home Network Watcher"
+    )
     parser.add_argument(
         "-c", "--config", help="Configuration file to use.", type=str, required=True
     )
@@ -79,19 +179,17 @@ def main():
         type=str,
         required=True,
     )
-    parser.add_argument(
-        "-o", "--output", help="file to write to", type=str, required=True
-    )
 
     parser.add_argument(
         "-l",
         "--last",
-        help="will run the report on the data from the last # of seconds specified",
-        type=int,
-    )
-
-    parser.add_argument(
-        "-q", "--quiet", help="will not print out status", action="store_true"
+        help="""will run the report on the data from the last # of seconds specified
+                you can specify an optional suffix (with no space):
+                  s: seconds (default)
+                  m: minutes (example: 32m)
+                  h: hours (example: 12h)
+                  d: days (example: 3d)""",
+        type=str,
     )
 
     args = parser.parse_args()
@@ -110,6 +208,11 @@ def main():
 
     dataSet = pd.read_csv(args.input)
 
+    if args.last:
+        success, dataSet = trimDataSetByLast(dataSet, args.last)
+        if False == success:
+            return  # error ocurred, message already printed
+
     targets = dataSet.columns[3:]
 
     if targets.size < 1:
@@ -117,7 +220,9 @@ def main():
         return
 
     for targ in targets:
-        doReport(dataSet, targ)
+        doReportForTarget(dataSet, targ)
+
+    reportPublicIP(dataSet)
 
 
 if __name__ == "__main__":
